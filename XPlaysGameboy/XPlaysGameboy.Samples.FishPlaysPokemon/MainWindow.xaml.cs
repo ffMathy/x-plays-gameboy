93e2ff01faaae1655316806b61aafaf20837c0f0
ffMathy <mathias.lorenzen@live.com>
2014-08-16 15:46:26 +0200

    upgraded gameboy engine.

--- XPlaysGameboy/XPlaysGameboy.Samples.FishPlaysPokemon/MainWindow.xaml.cs
+++ XPlaysGameboy/XPlaysGameboy.Samples.FishPlaysPokemon/MainWindow.xaml.cs
@@ -10,16 +10,24 @@ namespace XPlaysGameboy.Samples.FishPlaysPokemon
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly GameboyEngine _engine;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            _engine = new GameboyEngine();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
             //write the pokemon red ROM to the disk.
             var romPath = Path.Combine(Environment.CurrentDirectory, "PokemonRed.gb");
             File.WriteAllBytes(romPath, FileResources.PokemonRed);
 
-            var engine = new GameboyEngine();
-            engine.Start(romPath, RenderGrid);
+            _engine.Start(romPath, RenderGrid);
         }
     }
 }
--- XPlaysGameboy/XPlaysGameboy/GameboyEngine.cs
+++ XPlaysGameboy/XPlaysGameboy/GameboyEngine.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
 using PInvoke;
 using XPlaysGameboy.Properties;
+
+using DrawingPoint = System.Drawing.Point;
 
 namespace XPlaysGameboy
 {
@@ -16,12 +19,42 @@ namespace XPlaysGameboy
 
         public GameboyEngine()
         {
-            
+
         }
 
         public async void Start(string romLocation, FrameworkElement projectTo)
         {
+            Window projectWindow = null;
+
+            var parent = projectTo;
+            while (parent.Parent != null)
+            {
+                parent = (FrameworkElement)parent.Parent;
+                if (parent is Window)
+                {
+                    projectWindow = (Window)parent;
+                }
+            }
+
+            if (projectWindow == null)
+            {
+                throw new ArgumentException("The projection element must be a child of a window.", "projectTo");
+            }
+
+            foreach (var existingProcess in Process.GetProcessesByName("bgb"))
+            {
+                using (existingProcess)
+                {
+                    existingProcess.Kill();
+                    await Task.Delay(1000);
+                }
+            }
+
+            await Task.Delay(1000);
+
             var emulatorRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XPlaysGameboy", "Emulator");
+            Directory.CreateDirectory(emulatorRoot);
+
             var emulatorFilePath = Path.Combine(emulatorRoot, "bgb.exe");
             File.WriteAllBytes(emulatorFilePath, Resources.bgbexe);
             File.WriteAllText(Path.Combine(emulatorRoot, "bgb.ini"), Resources.bgbini);
@@ -29,38 +62,99 @@ namespace XPlaysGameboy
             var information = new ProcessStartInfo(emulatorFilePath);
             information.Arguments = "\"" + romLocation + "\"";
 
-            using (var process = Process.Start(information))
+            var process = Process.Start(information);
+
+            Debug.Assert(process != null, "process != null");
+            while (process.MainWindowHandle == IntPtr.Zero)
             {
+                await Task.Delay(1000);
+            }
 
-                Debug.Assert(process != null, "process != null");
-                while (process.MainWindowHandle == default(IntPtr))
-                {
-                    await Task.Delay(100);
-                }
+            await Task.Delay(1000);
 
-                var gameboyHandle = process.MainWindowHandle; 
+            var gameboyWindowHandle = NativeMethods.FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Tfgb", null);
 
-                var style = (long)NativeMethods.GetWindowLong(process.MainWindowHandle, NativeMethods.WindowLongFlags.GWL_STYLE);
-                style &= ~((uint)NativeMethods.SetWindowLongFlags.WS_CAPTION | (uint)NativeMethods.SetWindowLongFlags.WS_THICKFRAME | (uint)NativeMethods.SetWindowLongFlags.WS_MINIMIZE | (uint)NativeMethods.SetWindowLongFlags.WS_MAXIMIZE | (uint)NativeMethods.SetWindowLongFlags.WS_SYSMENU);
-                NativeMethods.SetWindowLong(gameboyHandle, NativeMethods.WindowLongFlags.GWL_STYLE, (NativeMethods.SetWindowLongFlags)style);
+            var style = (long)NativeMethods.GetWindowLong(process.MainWindowHandle, NativeMethods.WindowLongFlags.GWL_STYLE);
+            style &= ~((uint)NativeMethods.SetWindowLongFlags.WS_CAPTION | (uint)NativeMethods.SetWindowLongFlags.WS_THICKFRAME | (uint)NativeMethods.SetWindowLongFlags.WS_MINIMIZE | (uint)NativeMethods.SetWindowLongFlags.WS_MAXIMIZE | (uint)NativeMethods.SetWindowLongFlags.WS_SYSMENU);
+            style |= (uint)NativeMethods.SetWindowLongFlags.WS_EX_TOPMOST;
+            NativeMethods.SetWindowLong(gameboyWindowHandle, NativeMethods.WindowLongFlags.GWL_STYLE, (NativeMethods.SetWindowLongFlags)style);
 
+            var resizeProjection = (Action)delegate()
+            {
                 var projectionLocation = projectTo.PointToScreen(new Point(0, 0));
                 var projectionSize = new Size(projectTo.ActualWidth, projectTo.ActualHeight);
 
-                var resizeProjection = (Action) delegate()
-                {
+                NativeMethods.SetWindowPos(gameboyWindowHandle, new IntPtr(-1), (int)projectionLocation.X, (int)projectionLocation.Y,
+                    (int)projectionSize.Width, (int)projectionSize.Height, NativeMethods.SetWindowPosFlags.SWP_NOACTIVATE);
+            };
 
-                };
-
-                projectTo.SizeChanged += delegate(object sender, SizeChangedEventArgs args)
-                {
-                    projectionLocation = projectTo.PointToScreen(new Point(0, 0));
-                    projectionSize = new Size(projectTo.ActualWidth, projectTo.ActualHeight);
-                };
-
+            projectTo.SizeChanged += delegate
+            {
                 resizeProjection();
+            };
+            projectWindow.LocationChanged += delegate
+            {
+                resizeProjection();
+            };
+            projectWindow.SizeChanged += delegate
+            {
+                resizeProjection();
+            };
+            projectWindow.Closed += delegate
+            {
+                process.Kill();
+            };
 
-            }
+            resizeProjection();
+
+        }
+
+        public async void TapA()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("B");
+            });
+        }
+
+        public async void TapB()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("A");
+            });
+        }
+
+        public async void TapSelect()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("+");
+            });
+        }
+
+        public async void TapStart()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("{ENTER}");
+            });
+        }
+
+        public async void SaveState()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("{F2}");
+            });
+        }
+
+        public async void LoadState()
+        {
+            await Task.Run(delegate()
+            {
+                SendKeys.SendWait("{F4}");
+            });
         }
 
     }
